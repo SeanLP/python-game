610c782f39b542aea77b2e7ab63c3205d7895709
SeanLP <seanpreston9213@gmail.com>
2021-11-20 18:19:28 +0000

    Create hangman stages for game

--- run.py
+++ run.py
@@ -1,3 +1,57 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+# randomly selects game word froma list of words
+
+HANGMAN_STAGES = ['''
+    +-----+
+    |     |
+          |
+          |
+          |
+          |
+        ------ ''', '''
+    +-----+
+    |     |
+    O     |
+          |
+          |
+          |
+        ------ ''', '''
+    +-----+
+    |     |
+    O     |
+    |     |
+          |
+          |
+        ------ ''', '''
+    +-----+
+    |     |
+    O     |
+   /|     |
+          |
+          |
+        ------ ''', '''
+    +-----+
+    |     |
+    O     |
+   /|\    |
+          |
+          |
+        ------ ''', '''
+    +-----+
+    |     |
+    O     |
+   /|\    |
+   /      |
+          |
+        ------ ''', '''
+    +-----+
+    |     |
+    O     |
+   /|\    |
+   / \    |
+          |
+        ------ ''']
+print(HANGMAN_STAGES[6])
