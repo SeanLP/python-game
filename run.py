81bcb5d43396b0e33d7c6e663944f6c0044cfed1
SeanLP <seanpreston9213@gmail.com>
2021-11-27 17:33:25 +0000

    Add lives to game and check if user has lost

--- run.py
+++ run.py
@@ -1,5 +1,70 @@
 import random
 
+hangman_stages = ['''
+    +------+
+    |      |
+    O      |
+   /|\     |
+   / \     |
+           |
+           |
+       =========
+''', '''
+    +------+
+    |      |
+    O      |
+   /|\     |
+     \     |
+           |
+           |
+       =========
+''', '''
+    +------+
+    |      |
+    O      |
+   /|\     |
+           |
+           |
+           |
+       =========
+''', '''
+    +------+
+    |      |
+    O      |
+    |\     |
+           |
+           |
+           |
+       =========
+''', '''
+    +------+
+    |      |
+    O      |
+    |      |
+           |
+           |
+           |
+       =========
+''', '''
+    +------+
+    |      |
+    O      |
+           |
+           |
+           |
+           |
+       =========
+''', '''
+    +------+
+    |      |
+           |
+           |
+           |
+           |
+           |
+       =========
+''']
+
 word = ['superman', 'batman', 'joker']
 
 secret_word = random.choice(word)
@@ -8,10 +73,12 @@ display = []
 
 GAME_OVER = False
 
+lives = 6
+
 # Replaces each letter in secret word with dashes
 for letter in secret_word:
     display += '_'
-print(display)
+print(f"{' '.join(display)}")
 
 while not GAME_OVER:
     # Ask for user guess
@@ -23,9 +90,16 @@ while not GAME_OVER:
         if letter == guess:
             display[position] = letter
 
-    print(display)
+    if guess not in secret_word:
+        lives -= 1
+        if lives == 0:
+            GAME_OVER = True
+            print(f'Hard luck, you lost, the word was {secret_word}')
+
+    print(f"{' '.join(display)}")
 
     # Checks if there is no more dashes in secret word
     if '_' not in display:
         GAME_OVER = True
         print('Congratulations, you guessed the correct word and won!')
+
