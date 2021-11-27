3f04f6fb27604ab6611716ace5b5bae6500a4ed7
SeanLP <seanpreston9213@gmail.com>
2021-11-27 16:25:52 +0000

    Add code to replace secret word with dashes and check if letter guessed is in secret word

--- run.py
+++ run.py
@@ -1,73 +1,20 @@
 import random
-import time
 
-HANGMAN_STAGES = ['''
-    +-----+
-    |     |
-          |
-          |
-          |
-          |
-        ------ ''', '''
-    +-----+
-    |     |
-    O     |
-          |
-          |
-          |
-        ------ ''', '''
-    +-----+
-    |     |
-    O     |
-    |     |
-          |
-          |
-        ------ ''', '''
-    +-----+
-    |     |
-    O     |
-   /|     |
-          |
-          |
-        ------ ''', '''
-    +-----+
-    |     |
-    O     |
-   /|\    |
-          |
-          |
-        ------ ''', '''
-    +-----+
-    |     |
-    O     |
-   /|\    |
-   /      |
-          |
-        ------ ''', '''
-    +-----+
-    |     |
-    O     |
-   /|\    |
-   / \    |
-          |
-        ------ ''']
+word = ['superman', 'batman', 'joker']
 
-superHeroes = ['batman', 'superman', 'wonderwoman', 'aquaman', 'flash',
-               'darkseid', 'galactus', 'joker', 'bane', 'robin', 'thor',
-               'ironman', 'hulk', 'hawkeye', 'mystique', 'spiderman',
-               'thanos', 'deadpool']
+secret_word = random.choice(word)
 
-tolkienCharacters = ['frodo', 'bilbo', 'sam', 'merry', 'pippin',
-                     'gandalf', 'saruman', 'sauron', 'aragorn',
-                     'legolas', 'gimli', 'borimir', 'faramir',
-                     'gollum', 'shelob', 'balrog', 'lurtz']
+display = []
 
-name = input("Please enter your name ")
-print("Hello", name.capitalize(), "let's play Hangman!")
-time.sleep(.5)
-print("To win you must guess the secret word chosen by the computer.")
-time.sleep(.5)
-print("You can only guess one letter at a time,") 
-print("if you guess the wrong letter, the man will start to hang!")
-time.sleep(1)
-print("Let's play Hangman!")
+for letter in secret_word:
+    display += '_'
+print(display)
+
+guess = input('Guess a letter: ').lower()
+
+for position in range(len(secret_word)):
+    letter = secret_word[position]
+    if letter == guess:
+        display[position] = letter
+
+print(display)
