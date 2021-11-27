61ba5324653c178a2191b9abb408e78d015cdf8a
SeanLP <seanpreston9213@gmail.com>
2021-11-27 18:12:29 +0000

    Add heroes and villains to word list

--- run.py
+++ run.py
@@ -3,18 +3,9 @@ import random
 hangman_stages = ['''
     +------+
     |      |
-    O      |
-   /|\     |
-   / \     |
            |
            |
-       =========
-''', '''
-    +------+
-    |      |
-    O      |
-   /|\     |
-     \     |
+           |
            |
            |
        =========
@@ -22,7 +13,16 @@ hangman_stages = ['''
     +------+
     |      |
     O      |
-   /|\     |
+           |
+           |
+           |
+           |
+       =========
+''', '''
+    +------+
+    |      |
+    O      |
+    |      |
            |
            |
            |
@@ -40,7 +40,7 @@ hangman_stages = ['''
     +------+
     |      |
     O      |
-    |      |
+   /|\     |
            |
            |
            |
@@ -49,23 +49,32 @@ hangman_stages = ['''
     +------+
     |      |
     O      |
-           |
-           |
+   /|\     |
+     \     |
            |
            |
        =========
 ''', '''
     +------+
     |      |
-           |
-           |
-           |
+    O      |
+   /|\     |
+   / \     |
            |
            |
        =========
 ''']
 
-word = ['superman', 'batman', 'joker']
+word = ['superman', 'batman', 'joker', 'spiderman', 'wonderwoman', 'thor',
+        'hulk', 'daredevil', 'wolverine', 'aquaman', 'flash', 'deadpool',
+        'supergirl', 'antman', 'atom', 'batgirl', 'batwoman', 'catwoman',
+        'elektra', 'hawkeye', 'hellboy', 'ironman', 'marvelman', 'robin',
+        'wasp', 'watchmen', 'thanos', 'magneto', 'loki', 'darkseid',
+        'galactus', 'brainiac', 'doomsday', 'apocalypse', 'ultron',
+        'kingpin', 'scarecrow', 'deathstroke', 'venom', 'carnage',
+        'bane', 'bizarro', 'sinestro', 'mystique', 'deadshot', 'bullseye',
+        'zoom', 'penguin', 'abomination', 'dormammu', 'hobgoblin', 'riddler',
+        'lizard', 'sandman', 'clayface', 'mysterio', 'electro', 'morbius']
 
 secret_word = random.choice(word)
 
@@ -73,12 +82,13 @@ display = []
 
 GAME_OVER = False
 
-lives = 6
+lives = 0
 
 # Replaces each letter in secret word with dashes
 for letter in secret_word:
     display += '_'
 print(f"{' '.join(display)}")
+print()
 
 while not GAME_OVER:
     # Ask for user guess
@@ -90,9 +100,10 @@ while not GAME_OVER:
         if letter == guess:
             display[position] = letter
 
+    # If guess is incorrect, user's lives increase by 1, if 6 game ends
     if guess not in secret_word:
-        lives -= 1
-        if lives == 0:
+        lives += 1
+        if lives == 6:
             GAME_OVER = True
             print(f'Hard luck, you lost, the word was {secret_word}')
 
@@ -103,3 +114,5 @@ while not GAME_OVER:
         GAME_OVER = True
         print('Congratulations, you guessed the correct word and won!')
 
+    # Displays current hangman stage    
+    print(hangman_stages[lives])
